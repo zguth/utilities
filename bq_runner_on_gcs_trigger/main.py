647cb88f45ebf80eb1f3ceb84b903b862090f5e0
Zoltán Guth <zoltan.guth@doctusoft.com>
2019-08-30 10:36:30 +0200

    file name fix

--- bq_runner_on_gcs_trigger/main.py
+++ bq_runner_on_gcs_trigger/main.py
@@ -4,14 +4,17 @@ from google.cloud import bigquery
 # GCP libraries
 client = bigquery.Client()
 
-def run_sql_scripts(data, context):
-    print("Run triggered by file: {}.".format(data['objectId']))
+def load_dapo(data, context):
+    print("Run triggered by file: {}.".format(getFileLink(data)))
 
     for sql in readFilesFromDir('sql'):
         query_job = client.query(sql)  # API request
         query_job.result()  # Waits for query to finish
 
-    print("Processing finished for file: {}.".format(data['objectId']))
+    print("Processing finished for file: {}.".format(getFileLink(data)))
+
+def getFileLink(data):
+    return "gs://" + data['bucket'] + "/" + data['name']
 
 def readFilesFromDir(dir):
     files = []
